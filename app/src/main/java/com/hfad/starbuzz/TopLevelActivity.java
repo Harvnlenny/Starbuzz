8db171e1a7fc4d337eb9cfb51cff9f344eee7200
Aaron Harvey <harvnlenny@gmail.com>
2016-02-14 09:50:54 -0500

    Added onClickItemListener to topLevelActivity.java

--- app/src/main/java/com/hfad/starbuzz/TopLevelActivity.java
+++ app/src/main/java/com/hfad/starbuzz/TopLevelActivity.java
@@ -1,9 +1,13 @@
 package com.hfad.starbuzz;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class TopLevelActivity extends ActionBarActivity {
 
@@ -11,6 +15,18 @@ public class TopLevelActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+        //Create an OnItemClickListener
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener(){
+            public void onItemClick(AdapterView<?> listview, View v, int position, long id){
+                if (position == 0) {
+                    Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        //Add the listener to the list view
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
     }
 
     @Override

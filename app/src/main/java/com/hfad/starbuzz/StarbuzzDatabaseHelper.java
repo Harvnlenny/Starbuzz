40181e31ddcf10f5e085ebdd752b6f9e8fa2b92a
Aaron Harvey <harvnlenny@gmail.com>
2016-02-23 17:11:47 -0500

    Add empty DRINK table

--- app/src/main/java/com/hfad/starbuzz/StarbuzzDatabaseHelper.java
+++ app/src/main/java/com/hfad/starbuzz/StarbuzzDatabaseHelper.java
@@ -16,7 +16,11 @@ class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("CREATE TABLE DRINK ("
+            + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + "NAME TEXT, "
+            + "DESCRIPTION TEXT, "
+            + "IMAGE_RESOURCE_ID INTEGER);");
     }
 
     @Override

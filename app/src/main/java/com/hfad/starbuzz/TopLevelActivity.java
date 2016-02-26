b1761b7c3f6fed7a77cc61851db3995921a48c27
Aaron Harvey <harvnlenny@gmail.com>
2016-02-25 22:28:36 -0500

    Updated toplevel activity so that when we flag a drink as a favorite it appears under your favorite drinks.

--- app/src/main/java/com/hfad/starbuzz/TopLevelActivity.java
+++ app/src/main/java/com/hfad/starbuzz/TopLevelActivity.java
@@ -1,53 +1,110 @@
 package com.hfad.starbuzz;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
-public class TopLevelActivity extends ActionBarActivity {
+public class TopLevelActivity extends Activity {
+
+    private SQLiteDatabase db;
+    private Cursor favoritesCursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
         //Create an OnItemClickListener
-        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener(){
-            public void onItemClick(AdapterView<?> listview, View v, int position, long id){
-                if (position == 0) {
-                    Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
-                    startActivity(intent);
-                }
-            }
-        };
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener() {
+                    public void onItemClick(AdapterView<?> listView, View v, int position, long id) {
+                        if (position == 0) {
+                            Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+                };
         //Add the listener to the list view
         ListView listView = (ListView) findViewById(R.id.list_options);
         listView.setOnItemClickListener(itemClickListener);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_top_level, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        //Populate the list_favorites ListView from a cursor
+        ListView listFavorites = (ListView) findViewById(R.id.list_favorites);
+        try
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        {
+            SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+            db = starbuzzDatabaseHelper.getReadableDatabase();
+            favoritesCursor = db.query("DRINK",
+                    new String[]{"_id", "NAME"},
+                    "FAVORITE = 1",
+                    null, null, null, null, null);
+            CursorAdapter favoriteAdapter =
+                    new SimpleCursorAdapter(TopLevelActivity.this,
+                            android.R.layout.simple_list_item_1,
+                            favoritesCursor,
+                            new String[]{"NAME"},
+                            new int[]{android.R.id.text1}, 0);
+            listFavorites.setAdapter(favoriteAdapter);
+        } catch (
+                SQLiteException e
+                )
+
+        {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
         }
 
-        return super.onOptionsItemSelected(item);
+        //Navigate to DrinkActivity if a drink is clicked
+        listFavorites.setOnItemClickListener(new AdapterView.OnItemClickListener()
+
+                                             {
+                                                 @Override
+                                                 public void onItemClick(AdapterView<?> listView, View v, int position, long id) {
+                                                     Intent intent = new Intent(TopLevelActivity.this, DrinkActivity.class);
+                                                     intent.putExtra(DrinkActivity.EXTRA_DRINKNO, (int) id);
+                                                     startActivity(intent);
+                                                 }
+
+                                             }
+
+        );
+    }
+
+    //Close the cursor and database in the onDestroy() method
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        favoritesCursor.close();
+        db.close();
+    }
+
+    public void onRestart(){
+        super.onRestart();
+        try{
+            StarbuzzDatabaseHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+            db = starbuzzDatabaseHelper.getReadableDatabase();
+            Cursor newCursor = db.query("DRINK",
+                    new String[] {"_id", "NAME"},
+                    "FAVORITE = 1",
+                    null, null, null, null, null);
+            ListView listFavorites = (ListView)findViewById(R.id.list_favorites);
+            CursorAdapter adapter = (CursorAdapter) listFavorites.getAdapter();
+            adapter.changeCursor(newCursor);
+            favoritesCursor = newCursor;
+        } catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
